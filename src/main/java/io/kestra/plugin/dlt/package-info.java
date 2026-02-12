53d85f596b85ace2a4bd3ffcfd532b6f221ff66c
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-12 15:40:02 +0100

    chore: update plugin categories (#21)
    * chore: update plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>

--- src/main/java/io/kestra/plugin/dlt/package-info.java
+++ src/main/java/io/kestra/plugin/dlt/package-info.java
@@ -2,7 +2,7 @@
     title = "Dlt (Data Load Tool)",
     description = "This sub-group of plugins contains tasks to interact with dlt (data load tool).\n" +
         "dlt is an open-source library for declaratively loading data from APIs, databases, and files into data warehouses and lakes.",
-    categories = { PluginSubGroup.PluginCategory.INGESTION, PluginSubGroup.PluginCategory.TRANSFORMATION }
+        categories = { PluginSubGroup.PluginCategory.DATA }
 )
 package io.kestra.plugin.dlt;
 
